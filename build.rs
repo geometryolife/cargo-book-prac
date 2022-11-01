e43eeb73024aceeddc44b3dda5436e9f6b085e72
Joe Chen <geometryolife@gmail.com>
2022-11-01 14:01:00 +0800

    add some codes

--- build.rs
+++ build.rs
@@ -2,8 +2,7 @@
 fn main() {
     // Tell Cargo that if the given file changes, to return this build script.
     println!("cargo:rerun-if-changed=src/hello.c");
+    println!("cargo:warning=nice boy!");
     // Use the `cc` crate to build a C file and statically link it.
-    cc::Build::new()
-        .file("src/hello.c")
-        .compile("hello good good good");
+    cc::Build::new().file("src/hello.c").compile("hello good");
 }
