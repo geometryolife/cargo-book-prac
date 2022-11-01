8afd49de5590b7e525078a2a521ecbebd29b2aad
Joe Chen <geometryolife@gmail.com>
2022-11-01 20:07:43 +0800

    example1: code generation

---
+++ hello-from-generated-code/build.rs
@@ -0,0 +1,18 @@
+// Example in The Cargo Book
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("hello.rs");
+    fs::write(
+        &dest_path,
+        "pub fn message() -> &'static str {
+            \"Hello, Joe!\"
+        }
+        ",
+    )
+    .unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+}
