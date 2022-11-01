d6788f330a714d55a207a64c25ff0abb98baccd5
Joe Chen <geometryolife@gmail.com>
2022-11-01 17:09:12 +0800

    add a generation example

--- build.rs
+++ build.rs
@@ -8,6 +8,7 @@
 // }
 
 // Example in The Cargo Book
+/*
 use std::env;
 use std::fs;
 use std::path::Path;
@@ -24,4 +25,32 @@ fn main() {
     )
     .unwrap();
     println!("cargo:rerun-if-changed=build.rs");
+}
+*/
+
+// Building a native library
+// hello-world-from-c
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    // Note that there are a number of downsides to this approach, the comments
+    // below detail how to improve the portability of these commands;
+    Command::new("gcc")
+        .args(&["src/hello.c", "-c", "-fPIC", "-o"])
+        .arg(&format!("{}/hello.o", out_dir))
+        .status()
+        .unwrap();
+    Command::new("ar")
+        .args(&["crus", "libhello.a", "hello.o"])
+        .current_dir(&Path::new(&out_dir))
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=static=hello");
+    println!("cargo:rerun-if-changee=src/hello.c");
 }
